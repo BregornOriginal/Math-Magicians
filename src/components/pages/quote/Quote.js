8e253a5b58658a37ff65394d84221930ceaaff43
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-06 14:06:50 -0300

    Added styles for quote component

--- src/components/pages/quote/Quote.js
+++ src/components/pages/quote/Quote.js
@@ -1,8 +1,10 @@
+import styles from './Quote.module.css';
+
 export default function Quote() {
   return (
     <main>
       <div>
-        <p>
+        <p className={styles.quote}>
           Mathematics is not about numbers, equations, computations
           or algorithms: it is about understanding. -William Paul Thurston
         </p>
